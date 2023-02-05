8cc19991a7276a0a830580c7c6cc183e8157edde
TestProgger <gamersteam_dubki@mail.ru>
2023-02-05 22:30:26 +0300

    added key columns

--- api/model.py
+++ api/model.py
@@ -1,10 +1,21 @@
 from peewee import SqliteDatabase , Model
-from peewee import AutoField , TextField , IntegerField
-
-class User(Model):
-    id = AutoField(primary_key=True)
-    client_id = IntegerField()
-    host_id = IntegerField()
+from peewee import AutoField , TextField , IntegerField , DateTimeField
+from datetime import datetime
 
 db = SqliteDatabase('db.sqlite3')
-db.create_tables([User , ])
+
+class Client(Model):
+    id = AutoField(primary_key=True)
+    name = TextField(null=False)
+    client_id = IntegerField( null=False)
+    host_id = IntegerField( null=False)
+
+    private_key = TextField(null=False)
+    public_key = TextField(null=False)
+    pre_shared_key = TextField(null=False)
+
+    created_at = DateTimeField(default=datetime.now)
+
+    class Meta:
+        database = db
+
