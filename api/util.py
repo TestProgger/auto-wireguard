652365e36049bc677b9f8be8efaeb6e519949f70
TestProgger <gamersteam_dubki@mail.ru>
2023-02-06 06:43:10 +0300

    Added to exec_command stdin

--- api/util.py
+++ api/util.py
@@ -1,8 +1,11 @@
 import subprocess
 
 
-def exec_command(args : list[str]) -> str :
-    return subprocess.run(args , stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+def exec_command(args : list[str] , _input : str = None) -> str :
+    if _input is None:
+        return subprocess.run(args , stdout=subprocess.PIPE).stdout.decode("utf-8").strip()
+    else:
+        return subprocess.run(args , stdout=subprocess.PIPE , input=_input).stdout.decode("utf-8").strip()
 
 def get_server_keypair():
     privkey = exec_command(["cat" , "/etc/wireguard/privatekey"])
